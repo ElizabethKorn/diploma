3effbafdf4427983b2f18dee377ae87bb1604e40
Liza <lkorn036@gmail.com>
2025-10-23 17:47:30 -0500

    Fix: Replace BrowserRouter with HashRouter for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import TicketsPage from "./pages/TicketPage/TicketsPage";
 import "./App.css";
